3d68547b72bc338627decafb971208fff39dce9d
KelinSasha <KelinSashaACWG@gmail.com>
2021-03-03 16:16:21 -0800

    Update

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -14,13 +14,12 @@ function statusChangeCallback(response) {
   if (response.status === 'connected') {
     // Logged into your app and Facebook.
         console.log('Successfully logged in with Facebook');
-         FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
+        FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
   }
 }
 function changeUser(response) {
   console.log(response);
-
-  $(".facebookLogin").hide();
-  $("#name").text(response.name);
-  $("#photo").attr("src",response.picture.data.url);
+  $(".loginPage").fadeOut();
+  $(".userGuide").fadeIn();
+  $("#important").text("Hi, " + response.name + "!");
 }
